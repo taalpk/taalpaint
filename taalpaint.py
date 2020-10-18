2b96a6500b0b3b5a066eb46c56eb6dd4eeb86c85
Muhammad Talha <m.talhaimran812@gmail.com>
2020-10-18 20:35:46 +0500

    updated TAAL paint to add lines , icon bar,colors.

--- taalpaint.py
+++ taalpaint.py
@@ -1,167 +1,124 @@
-import tkinter
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
-from tkinter import ttk
+from PIL import Image,ImageTk
 
-window=tkinter.Tk()
+window=Tk()
 window.title("TAAL Paint")
 
-
-
-#def new_tab(event):
-#    tab=ttk.Frame(tab_control)
-#    tab_control.add(tab,text="Untitled File")
-#    tab_control.pack(expand="yes",fill="both")
-#    canvas=tkinter.Canvas(tab,bg="white")
-#    canvas.pack(expand="yes",fill="both")
-
-def save_file():
-    filedialog.asksaveasfile(mode="w")
+def close():
+    exit()
 
 def open_file():
     result=filedialog.askopenfile(title="Select File Directory")
     for i in result:
         print(i)
 
-def close():
-    exit()
+def save_file():
+    f=filedialog.asksaveasfile(mode="w",filetypes=(("text files",".txt"),("all files",".*")))
+    file = input("enter lines : ")
+    f.write(file)
+    f.close()
 
 def help():
-    tkinter.messagebox.showinfo("Help","Muhammad Asad Ullah")
+    messagebox.showinfo("Help","Call 1122")
 
 def dev_info():
-    tkinter.messagebox.showinfo("Developer's Information","This Program is Developed By TAAL")
-
-main_menu=tkinter.Menu(window,tearoff=0)
-
-file_menu=tkinter.Menu(main_menu,tearoff=0)
-
-file_menu.add_command(label="New File")
-
-file_menu.add_command(label="Open File",command=open_file)
-
-file_menu.add_command(label="Save File",command=save_file)
-
+    messagebox.showinfo("Developer's Information","This Program is Developed By TAAL")
+    
+main_menu = Menu(window,tearoff=0)
+file_menu = Menu(main_menu,tearoff=0)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Open",command=open_file)
+file_menu.add_command(label="Save",command=save_file)
 file_menu.add_separator()
-
 file_menu.add_command(label="Exit",command=close)
-
 main_menu.add_cascade(label="File",menu=file_menu)
 
-main_menu.add_command(label="Help",command=help)
-
-main_menu.add_separator()
-
-main_menu.add_command(label="About",command=dev_info)
-
-icon_frame=tkinter.Frame(window)
-icon_frame.pack(fill="both")
-
-new_file=tkinter.Button(icon_frame,text="New File")
-new_file.pack(side="left")
-
-open_file=tkinter.Button(icon_frame,text="Open File",command=open_file)
-open_file.pack(side="left")
-
-save_file=tkinter.Button(icon_frame,text="Save File",command=save_file)
-save_file.pack(side="left")
-
-exit_file=tkinter.Button(icon_frame,text="Exit",command=close)
-exit_file.pack(side="right")
-
-#tab_control=ttk.Notebook(window)
-
-#tab=ttk.Frame(tab_control)
-
-#tab_control.add(tab,text="Untitled File")
-#tab_control.pack(expand="yes",fill="both")
-
-canvas=tkinter.Canvas(window,bg="white")
-canvas.pack(expand="yes",fill="both")
-
-tool_frame=tkinter.LabelFrame(window,text="Tools")
-tool_frame.pack(fill="both")
-
-def clrscr():
-    canvas.delete("all")
-
-color = 0
-
-def color_red():
-    global color
-    color = "red"
-
-def color_green():
-    global color
-    color = "green"
-
-def color_blue():
-    global color
-    color = "blue"
-
-def pen(event):
-    x1=event.x
-    y1=event.y
-    x2=event.x
-    y2=event.y
-    canvas.create_oval(x1,y1,x2,y2,fill=color)
-
-button1=tkinter.Button(window,text="Red",bg="red",fg="white",command=color_red)
-canvas.bind("<B1-Motion>",pen)
-button1.pack(side="left")
-
-button2=tkinter.Button(window,text="Green",bg="green",fg="white",command=color_green)
-canvas.bind("<B1-Motion>",pen)
-button2.pack(side="left")
-
-button3=tkinter.Button(window,text="Blue",bg="blue",fg="white",command=color_blue)
-canvas.bind("<B1-Motion>",pen)
-button3.pack(side="left")
-
-clear_screen=tkinter.Button(tool_frame,text="Clear Screen",command=clrscr)
-clear_screen.pack(side="left")
-
+help_menu = Menu(main_menu,tearoff=0)
+help_menu.add_command(label="About",command=dev_info)
+help_menu.add_separator()
+help_menu.add_command(label="Help",command=help)
+main_menu.add_cascade(label="Help",menu=help_menu)
 window.config(menu=main_menu)
 
-#from PIL import ImageTk,Image
+iconbar = LabelFrame(window,text="IconBar",bd=4,relief=RIDGE)
+iconbar.pack(side="top",fill="both")
 
-#img=ImageTk.PhotoImage(Image.open("unnamed.jpg"))
-#my_label=Label(exit_file,image=img)
-#my_label.pack()
+img1=ImageTk.PhotoImage(Image.open("file_icon.png"))
+icon1 = Button(iconbar,text="",image=img1)
+icon1.pack(side="left")
 
-#img=ImageTk.PhotoImage(Image.open("unnamed.png"))
-#my_label=Label(open_file,image=img)
-#my_label.pack()
+img2=ImageTk.PhotoImage(Image.open("folder-icon.png"))
+icon2 = Button(iconbar,text="",image=img2,command=open_file)
+icon2.pack(side="left")
 
-#window.bind("<Control_L><n>")
-#window.bind("<Control_R><n>")
-#window.bind("<Control_L><N>")
-#window.bind("<Control_R><N>")
+img3=ImageTk.PhotoImage(Image.open("floppy_icon.png"))
+icon3 = Button(iconbar,text="",image=img3,command=save_file)
+icon3.pack(side="left")
 
-#window.bind("<Control_L><o>")
-#window.bind("<Control_R><o>")
-#window.bind("<Control_L><O>")
-#window.bind("<Control_R><O>")
+scale = Scale(window,from_=20,to=0,orient='vertical',tickinterval=1)
+scale.pack(side='left',fill="y")
+scale.get()
 
-#window.bind("<Control_L><s>")
-#window.bind("<Control_R><s>")
-#window.bind("<Control_L><S>")
-#window.bind("<Control_R><S>")
+    
+def erase():
+    global color
+    color = "white"
 
-#window.bind("<Control_L><d>")
-#window.bind("<Control_R><d>")
-#window.bind("<Control_L><D>")
-#window.bind("<Control_R><D>")
+def clear():
+    canvas.delete("all")
 
-#window.bind("<Control_L><q>",close)
-#window.bind("<Control_R><q>",close)
-#window.bind("<Control_L><Q>",close)
-#window.bind("<Control_R><Q>",close)
+x1 = 0
+y1 = 0
 
-#window.bind("<Alt_L><F4>",close)
-#window.bind("<Alt_R><F4>",close)
-#window.bind("<Alt_L><F4>",close)
-#window.bind("<Alt_R><F4>",close)
+color = 'black'
+
+def point_1(event):
+    global x1
+    global y1    
+    x1 = event.x
+    y1 = event.y
+
+def movement(event):
+    global x1
+    global y1
+    x2=event.x
+    y2=event.y
+    canvas.create_line((x1,y1,x2,y2),fill=color,width=scale.get())
+    x1 = event.x
+    y1 = event.y
+
+def line_color(new_color):
+    global color
+    color = new_color   
+
+canvas=Canvas(window,bg="white")
+canvas.pack(expand="yes",fill="both")
+
+colorFrame=LabelFrame(window,text="Colors",bd=4)
+colorFrame.pack(side="left")
+
+redButton = Button(colorFrame,text="",bg="red",width=2,command=lambda : line_color('red'))
+canvas.bind("<Button-1>",point_1)
+canvas.bind("<B1-Motion>",movement)
+redButton.pack(side="left")
+
+greenButton = Button(colorFrame,text= "",bg="green",width=2,command=lambda : line_color('green'))
+canvas.bind("<Button-1>",point_1)
+canvas.bind("<B1-Motion>",movement)
+greenButton.pack(side="left")
+
+blueButton = Button(colorFrame,text= "",bg="blue",width=2,command=lambda : line_color('blue'))
+canvas.bind("<Button-1>",point_1)
+canvas.bind("<B1-Motion>",movement)
+blueButton.pack(side="left")
+
+eraseButton = Button(window,text="Erase",height=2,command=erase)
+canvas.bind("<B1-Motion>",movement)
+eraseButton.pack(side="right")
+
+clearButton = Button(window,text="Clear",height=2,command=clear)
+clearButton.pack(side="right")
 
 window.mainloop()
